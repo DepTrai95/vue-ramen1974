8325c78b4986020d3140a8ddd616c41b56ee59f5
Duc Anh Dennis Nguyen <dennidnguyen@yahoo.de>
2024-08-26 22:56:26 +0200

    [TASK] update images (smaller size) + update sitemap

--- src/components/gallery/Gallery.vue
+++ src/components/gallery/Gallery.vue
@@ -4,7 +4,7 @@
          <source type="image/webp"
             :srcset="`/img/${fileName}-small.webp 400w, /img/${fileName}-medium.webp 600w, /img/${fileName}-large.webp 1200w`"
             sizes="(max-width: 599px) 400px, (max-width: 899px) 600px, 1200px" />
-         <img :src="`/img/${fileName}-large.webp`" height width="300" loading="lazy" alt="" />
+         <img :src="`/img/${fileName}-large.webp`" height="450" width="300" loading="lazy" alt="" />
       </picture>
    </div>
 </template>
