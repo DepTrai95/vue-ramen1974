e2d05fcfa863c9a2b210557de2ecc273fc4dd2f5
Duc Anh Nguyen <53148787+DepTrai95@users.noreply.github.com>
2024-10-22 22:19:15 +0200

    [UPDATE] add meta-tag to 404 page

--- src/pages/NotFound.vue
+++ src/pages/NotFound.vue
@@ -5,7 +5,6 @@
             <h2>Whooops</h2>
             <p>
                Diese Seite konnte leider nicht gefunden werden. Sie könnte gelöscht worden sein
-               Zurück zur
             </p>
             <router-link to="/" class="btn--primary">← Zurück zur Homepage</router-link>
          </div>
@@ -13,6 +12,16 @@
    </section>
 </template>
 
+<script>
+export default {
+   created() {
+      let meta = document.createElement('meta');
+      meta.name = "robots";
+      meta.content = "noindex";
+      document.getElementsByTagName('head')[0].appendChild(meta);
+   }
+}
+</script>
 <style lang="scss" scoped>
 section {
    margin-block: 7rem;
